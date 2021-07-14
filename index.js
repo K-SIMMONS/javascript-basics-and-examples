14b3f43c064a220120978d692ca66716e6a7a463
Kurt Simmons <ksimmons@alphaworks.tech>
2021-07-14 09:49:10 -0700

    added some javascript

--- index.js
+++ index.js
@@ -1,5 +1,67 @@
 //This is how you comment in javascript. it's just two forward slashes
 
+//Here we have the console log. The console log will display your values in the console portion of the inspect element tool on your browser
+//You can use this to keep track of data and place markers as you go
+//Writing within single(') or double("") will read whatever insert as Text
+console.log('Hello Kurt!');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//EXAMPLES////////////////////////////////////////////////////////////////
+
+
+//Here is a function that ask user to enter a number, then display whether or not that number is odd or even!
 function oddEvenBtn()
 {
     let num1 = prompt("Enter a number");
@@ -18,3 +80,4 @@ function oddOrEven(number)
         return "odd";
     }
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
